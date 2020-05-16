2bf8d905bc08e196a2e926793c4ad5bfbc16b105
julian-hecker <julianheckerdev@gmail.com>
2020-05-16 17:44:11 -0400

    add react build

---
+++ build/precache-manifest.0372f29862ff860fe26e6455e2d78315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e2ee36a028a9cfce9eb9d972113ba9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977dbfe9e1a3cd7ff9d5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2d216cb9435a110f5f55",
+    "url": "/static/css/main.48f44eb5.chunk.css"
+  },
+  {
+    "revision": "977dbfe9e1a3cd7ff9d5",
+    "url": "/static/js/2.a56fa522.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a56fa522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d216cb9435a110f5f55",
+    "url": "/static/js/main.77c0569b.chunk.js"
+  },
+  {
+    "revision": "0c734cd8d86f6e6c88f0",
+    "url": "/static/js/runtime-main.95d7e7e5.js"
+  }
+]);
