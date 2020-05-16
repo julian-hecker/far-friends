63dc14b62739c878290e8d3d7d808ffceabe423b
julian-hecker <julianheckerdev@gmail.com>
2020-05-16 03:02:20 -0400

    connect to database

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const connectionURI = process.env.CLEARDB_DATABASE_URL;
 
-const sequelize = new Sequelize(connectionURI);
+const db = new Sequelize(connectionURI, {
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
 
-module.exports = sequelize;
+module.exports = db;
