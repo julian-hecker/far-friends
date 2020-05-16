6a15729018c105ee4935bca2f33b610b3153ff42
julian-hecker <julianheckerdev@gmail.com>
2020-05-16 03:02:34 -0400

    attempt first api endpoint

---
+++ api/userRoutes.js
@@ -0,0 +1,45 @@
+const { Router } = require('express');
+const router = new Router(),
+    db = require('../db.js'),
+    User = require('../models/user.js');
+
+// Get All Users
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(users => {
+            res.send(users);
+        })
+        .catch(err => {
+            console.log('ERROR! ' + err);
+        });
+});
+
+
+// Get Single User by ID
+router.get('/:id', (req, res) => {
+    res.send('user with id ' + req.params.id);
+});
+
+// Create a user
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    let { userName, firstName, lastName, email, gender, country } = data;
+
+    User.create({
+        userName, 
+        firstName,
+        lastName,
+        email,
+        gender,
+        country,
+    }).then(user => {
+        res.json(user);
+    }).catch(err => {
+        res.send(err);
+    });
+})
+
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 // === Packages ===
-const express = require('express'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local');
-    
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local");
+
 // App Config
 const app = express();
-app.set('view engine', 'html');
-app.use(express.static(__dirname + '/build'));
-app.set('views', __dirname + '/build');
+const db = require("./db.js");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.set("view engine", "html");
+app.use(express.static(__dirname + "/build"));
+app.set("views", __dirname + "/build");
 
 // === Routes ===
-// const api = routes.js
-// app.use('/api', api);
-app.get('*', (req, res) => {
-    res.render('index.html');
+app.use("/api/users", require("./api/userRoutes.js"));
+app.get("*", (req, res) => {
+    res.render("index.html");
 });
-
 
 // === Run Server ===
 const PORT = process.env.PORT || 3000;
