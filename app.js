69a073b1ccae5a3a1752197c937bb0d2891341d8
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2020-05-16 20:10:37 -0400

    Added CORS headers

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const express = require("express"),
 
 // App Config
 const app = express();
-const db = require("./db.js");
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	if(req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', '*');
+		return res.status(200).json({});
+	}
+	next();
+})
+
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.json());
 app.set("view engine", "html");
